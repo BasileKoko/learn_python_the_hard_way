5db443e67a7aeac27440b4d8859a47c2b4922b0c
Basile Koko <basile_koko@hotmail.com>
2017-06-10 18:02:38 +0100

    reviewing modules

---
+++ Exercises_06/mod.py
@@ -0,0 +1,14 @@
+class Mod:
+
+	def call_review(self):
+		import review
+		user = review.Review("basile")
+		return user.print_name()
+
+	def call_holla(self):
+		import holla
+		return holla.username()
+
+mod = Mod()
+print mod.call_review()
+print mod.call_holla()
---
+++ Exercises_06/review.py
@@ -0,0 +1,7 @@
+class Review:
+	def __init__(self, name):
+		self.name = name
+
+	def print_name(self):
+		return self.name + " is this the way?"
+
