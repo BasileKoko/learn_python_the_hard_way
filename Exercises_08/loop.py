0b53eb3d4ac429672d377cf47f05c212bbe96aaf
Basile Koko <basile_koko@hotmail.com>
2017-06-11 10:06:09 +0100

    reviewing loop

---
+++ Exercises_08/loop.py
@@ -0,0 +1,24 @@
+#for loop
+def loop1():
+  primes = [2,3,4]
+  for i in primes:
+    print(i)
+
+loop1()
+
+# for loop with range
+
+def loop2():
+    for i in range(3):
+      print(i)
+
+loop2()
+
+# while loop
+def loop3():
+    count = 0
+    while count <= 3:
+        print(count)
+        count += 1
+
+loop3()
