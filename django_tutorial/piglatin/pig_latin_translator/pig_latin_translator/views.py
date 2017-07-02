015328bac7d4bab2f985707e2c18a75181fcd2a7
Basile Koko <basile_koko@hotmail.com>
2017-07-02 19:25:52 +0100

    adding translate.html to templates

--- django_tutorial/piglatin/pig_latin_translator/pig_latin_translator/views.py
+++ django_tutorial/piglatin/pig_latin_translator/pig_latin_translator/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello(request):
+def home(request):
     return render(request, 'home.html')
+
+def translate(request):
+    # return render(request, 'translate.html')
+    return HttpResponse("You reached the translate page!" + request.GET['data'])
