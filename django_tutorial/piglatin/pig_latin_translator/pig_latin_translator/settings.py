2727f85ae72688a70ec80a5e1cc0284a20e1534d
Basile Koko <basile_koko@hotmail.com>
2017-07-02 18:09:57 +0100

    adding templates

--- django_tutorial/piglatin/pig_latin_translator/pig_latin_translator/settings.py
+++ django_tutorial/piglatin/pig_latin_translator/pig_latin_translator/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pig_latin_translator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
