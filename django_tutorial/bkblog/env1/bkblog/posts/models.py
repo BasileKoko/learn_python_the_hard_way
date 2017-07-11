6370ee9ac2a2e1103d595c15807ef42e8e796873
Basile Koko <basile_koko@hotmail.com>
2017-07-11 13:35:48 +0100

    building posts app

--- django_tutorial/bkblog/env1/bkblog/posts/models.py
+++ django_tutorial/bkblog/env1/bkblog/posts/models.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # from __future__ import unicode_literals
 
-# from django.db import models
-#
-# # class Post(models.Model):
-# #     title = models.CharField(max_length=250)
-# #     created_date = models.DateTimeField()
-# #     # image = models.ImageFiled(upload_to='media/')
-# #     body = models.TextField()
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    created_date = models.DateTimeField()
+    image = models.ImageField(upload_to='media/')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
--- django_tutorial/bkblog/env1/bkblog/posts/views.py
+++ django_tutorial/bkblog/env1/bkblog/posts/views.py
@@ -2,6 +2,9 @@
 # from __future__ import unicode_literals
 
 from django.shortcuts import render
+from models import Post
+
 
 def home(request):
-    return render(request, 'posts/home.html')
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'posts/home.html', {'posts': posts})
