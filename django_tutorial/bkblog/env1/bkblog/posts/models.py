fe858ce9b18fedc5b521f74828add6757bb27b6f
Basile Koko <basile_koko@hotmail.com>
2017-07-11 11:34:55 +0100

    adding posts app

---
+++ django_tutorial/bkblog/env1/bkblog/posts/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# from __future__ import unicode_literals
+
+# from django.db import models
+#
+# # class Post(models.Model):
+# #     title = models.CharField(max_length=250)
+# #     created_date = models.DateTimeField()
+# #     # image = models.ImageFiled(upload_to='media/')
+# #     body = models.TextField()
