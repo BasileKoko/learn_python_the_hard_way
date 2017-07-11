345ab9a494e7736b27a45d3fdc159fcecc562b3b
Basile Koko <basile_koko@hotmail.com>
2017-07-11 09:24:58 +0100

    bkblog created

---
+++ django_tutorial/bkblog/env1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Basile/Desktop/makers_2017/projects/python_review/django_tutorial/bkblog/env1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
