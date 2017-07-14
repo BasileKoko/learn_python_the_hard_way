611ed348ed0bef72e2834d51a1b4585b7d5aec24
Basile Koko <basile_koko@hotmail.com>
2017-07-14 10:09:20 +0100

    adding generators with list comprehension

--- generators.py
+++ generators.py
@@ -5,3 +5,9 @@ def square_number(nums):
 my_nums = square_number([1,2,3,4,5])
 print(my_nums)
 print(next(my_nums))
+
+# Generators with list comprehension
+
+my_nums2 = (x*x for x in [4,6,3,7,2])
+print(my_nums2)
+print(next(my_nums2))
