21a6fcdc88cd3034d713838a87db74fa5322908d
Basile Koko <basile_koko@hotmail.com>
2017-07-17 09:37:36 +0100

    adding project redditclone

---
+++ django_2/redditclone/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Basile/Desktop/makers_2017/projects/python/django_2/redditclone/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
