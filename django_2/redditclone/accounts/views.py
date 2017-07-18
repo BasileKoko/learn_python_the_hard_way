45f065f3dc7362bb0319549b8e5e6bfa95c85c71
Basile Koko <basile_koko@hotmail.com>
2017-07-18 11:51:55 +0100

    handling login and signup

--- django_2/redditclone/accounts/views.py
+++ django_2/redditclone/accounts/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
 
@@ -12,7 +13,7 @@ def signup(request):
             try:
                 user = User.objects.get(username=request.POST['username'])
                 return render(request, 'signup.html', {'error': 'Username already taken'})
-            except user.DoesNotExist:
+            except User.DoesNotExist:
                 User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 return render(request, 'signup.html')
         else:
@@ -21,5 +22,13 @@ def signup(request):
         return render(request, 'signup.html')
 
 
-def login(request):
-    return render(request, 'login.html')
+def loginview(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return render(request, 'login.html', {'error': 'Successfully login'})
+        else:
+            return render(request, 'login.html', {'error':'username and password does not match'})
+    else:
+        return render(request, 'login.html')
