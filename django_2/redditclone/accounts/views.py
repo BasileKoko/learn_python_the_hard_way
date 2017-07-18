3e61d1f647e7897205cdffc75a84f6398727c047
Basile Koko <basile_koko@hotmail.com>
2017-07-18 10:07:03 +0100

    user uniqueness

--- django_2/redditclone/accounts/views.py
+++ django_2/redditclone/accounts/views.py
@@ -2,9 +2,24 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'signup.html', {'error': 'Username already taken'})
+            except user.DoesNotExist:
+                User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                return render(request, 'signup.html')
+        else:
+            return render(request, 'signup.html', {'error':'password does not match'})
+    else:
+        return render(request, 'signup.html')
+
 
 def login(request):
     return render(request, 'login.html')
