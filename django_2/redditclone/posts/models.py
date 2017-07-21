7d32c3f3c15f58db4758744ea831772376d852ab
Basile Koko <basile_koko@hotmail.com>
2017-07-21 12:00:39 +0100

    adding models Post

--- django_2/redditclone/posts/models.py
+++ django_2/redditclone/posts/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    url = models.TextField()
+    pub_date = models.DateTimeField()
+    author = models.ForeignKey(User)
+    votes_total = models.IntegerField(default=1)
